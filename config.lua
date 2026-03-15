2a7167d21ae66f5a008e0348e5a1e677b8ee77b0
Morpheus <brc4rr011@outlook.com>
2026-03-15 16:07:53 +0000

    change message

--- config.lua
+++ config.lua
@@ -12,13 +12,14 @@ CodeStudio.TaxiDestination = vector4(-1087.01, -271.25, 37.32, 26.0)         --T
 CodeStudio.SkipToNearestLoc = vector4(-1198.92, -304.74, 37.47, 284.08)       --Taxi Skip Nearest Location to destination
 
 
-CodeStudio.WelcomeMessage = 'Welocome To My Server'
+CodeStudio.WelcomeMessage = 'Welocome To Endless Dreams!'
 CodeStudio.ReachedMessage = "We've reached our destination"
 
 
 
 function Notify(msg)
-    SetNotificationTextEntry('STRING') --- DELETE ME IF YOU ARE USING ANOTHER SYSTEM
-    AddTextComponentString(msg)  --- DELETE ME IF YOU ARE USING ANOTHER SYSTEM
-    DrawNotification(0,1)  --- DELETE ME IF YOU ARE USING ANOTHER SYSTEM
+    -- SetNotificationTextEntry('STRING') --- DELETE ME IF YOU ARE USING ANOTHER SYSTEM
+    -- AddTextComponentString(msg)  --- DELETE ME IF YOU ARE USING ANOTHER SYSTEM
+    -- DrawNotification(0,1)  --- DELETE ME IF YOU ARE USING ANOTHER SYSTEM
+    TriggerEvent("QBCore:Notify", msg)
 end
